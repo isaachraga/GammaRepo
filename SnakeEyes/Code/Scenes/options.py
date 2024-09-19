bb5eefeb5f593108601eeb9d29d5e4ae43a1c22f
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-09-19 18:52:10 -0400

    Proposal for scene manager restructure

--- SnakeEyes/Code/Scenes/options.py
+++ SnakeEyes/Code/Scenes/options.py
@@ -2,9 +2,24 @@ import pygame
 from settings import Settings
 
 class Options:
-    def __init__(self, screen):
-        self.screen = screen
+    def __init__(self, scene_manager):
+        self.scene_manager = scene_manager
+        self.screen = self.scene_manager.screen
         self.GAME_FONT = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", Settings.FONT_SIZE)
+    
+    def run(self):
+        self.update()
+        self.render()
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    self.scene_manager.quit()
+
+            if event.type == pygame.KEYDOWN:
+                # Scene Selection
+                if event.key == pygame.K_s:
+                    self.scene_manager.switch_scene('scene')
 
     def render(self):
         self.screen.fill((255, 255, 255))
