bbc16a25a4082b6f9028906db40fc79403e394a0
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-09-26 20:38:04 -0400

    Added Game Setup Scene

---
+++ SnakeEyes/Code/preferences.py
@@ -0,0 +1,17 @@
+#Preferences decided when in game setup scene
+#Edit to change default options
+
+class Preferences:
+    FINISHLINE_SCORE = 200
+
+    #Player types: Player, CPU, or None
+    RED_PLAYER_TYPE = "Player"
+    BLUE_PLAYER_TYPE = "Player"
+    YELLOW_PLAYER_TYPE = "Player"
+    GREEN_PLAYER_TYPE = "Player"
+
+    #Player control schemes
+    RED_CONTROLS = "WASD"
+    BLUE_CONTROLS = "TFGH"
+    YELLOW_CONTROLS = "IJKL"
+    GREEN_CONTROLS = "Arrows"
