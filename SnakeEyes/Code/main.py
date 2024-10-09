fbded2413b84b75be31622147bd84fe34509c47f
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-10-09 13:36:38 -0400

    Changed file pathing

--- SnakeEyes/Code/main.py
+++ SnakeEyes/Code/main.py
@@ -1,7 +1,16 @@
 #Makes file imports able to be done from the root
 #This makes file paths compatible with the paths our tests expect
-import sys, os
-sys.path.append(os.path.abspath('')) 
+import sys
+import os
+
+#Get the absolute path of the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+#Set GammaRepo's path
+gamma_repo_path = os.path.abspath(os.path.join(current_dir, '..', '..'))
+#Change the working directory to GammaRepo
+os.chdir(gamma_repo_path)
+print(f"Working directory set to: {os.getcwd()}") #Debug
+sys.path.append(gamma_repo_path)
 
 import pygame
 from SnakeEyes.Code.scene_manager import SceneManager
