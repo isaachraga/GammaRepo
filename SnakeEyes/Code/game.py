161932c682e7013caccdc5e923b9f17a47d0c3d5
isaachraga <isaachraga@yahoo.com>
2024-10-14 11:13:47 -0400

    fixed bug that automatically tranistions if last round and police or all stores alarmed

--- SnakeEyes/Code/game.py
+++ SnakeEyes/Code/game.py
@@ -10,8 +10,6 @@ from SnakeEyes.Code.preferences import Preferences
 
 
 ### BUGS ###
-# in game status didnt update on last round information
-# last round and police are interfering
 # need to swap out space trigger for dice rolling, needs to work with controller and changed in test sim key
 # store collision needs fixed
 # dont set off police on first alarm
@@ -44,7 +42,7 @@ class Game:
     def initialization(self):
         ### Flags and General Game Vars ###
         self.dt = 0
-        self.result = ""
+        self.result = "init"
         self.winScore = Preferences.FINISHLINE_SCORE
         self.lastRound = False
         self.gameOverFlag = False
@@ -296,8 +294,8 @@ class Game:
         #self.GAME_FONT.render_to(self.screen, (10, 540), "P1: "+str(self.p1.score)+"   P2: "+str(self.p2.score)+"   P3: "+str(self.p3.score)+"   P4: "+str(self.p4.score), (0, 0, 0))
         
         #self.GAME_FONT.render_to(self.screen, (350, 20), "HIGHEST SCORE PAST "+str(self.winScore)+" WINS", (0, 0, 0))
-        if self.lastRound:
-            self.GAME_FONT.render_to(self.screen, (350, 50), self.result, (0, 0, 0))
+        #if self.lastRound:
+        self.GAME_FONT.render_to(self.screen, (1100, 690), self.result, (255,255,255))
 
 
 
@@ -598,7 +596,7 @@ class Game:
 
                                             s.players.clear()
                                         else:
-                                            self.result = ""
+                                            self.result = "Roll Default"
                                             for p in s.players:
                                                 if p.status == 1:
                                                     p.tmpScore = p.tmpScore+self.award
@@ -781,8 +779,8 @@ class Game:
                 p.status = -1
         if not self.lastRound:
             self.result = "SNAKE EYES"
-        else:
-            self.gameOver()
+        #else:
+            #self.gameOver()
 
     def resetTempScores(self):
         for p in self.Players:
@@ -825,7 +823,7 @@ class Game:
         self.dt = 0
         self.num1 = 0
         self.num2 = 0
-        self.result = ""
+        self.result = "RoundReset"
         self.allAlarms = False
         self.police = False
         self.alarmedStores = 0
@@ -840,7 +838,7 @@ class Game:
         self.dt = 0
         self.num1 = 0
         self.num2 = 0
-        self.result = ""
+        self.result = "GameReset"
         self.lastRound = False
         self.allAlarms = False
         self.police = False
