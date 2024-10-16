09fece62fc559787c53067ea94216caadd2c0308
Nick Neitenbach <nneiten1@kent.edu>
2024-10-16 16:20:52 -0400

    Update test_assets.py
    added tests for lucky streak and hot dice modifiers. will clean up once all modifiers are finished

--- SnakeEyes/Tests/test_assets.py
+++ SnakeEyes/Tests/test_assets.py
@@ -63,3 +63,11 @@ def test_icon_assets():
 def test_GUI_theme():
     logging.info("Testing GUI Theme")
     assert os.path.exists("SnakeEyes/Assets/theme.json")
+
+def test_hot_dice():
+    logging.info("Tesing hot dice icon")
+    assert os.path.exists("SnakeEyes/Assets/Icons/hotDiceModifier.png")
+
+def test_lucky_streak():
+    logging.info("Tesing lucky streak icon")
+    assert os.path.exists("SnakeEyes/Assets/Icons/luckyStreakModifier.png")
