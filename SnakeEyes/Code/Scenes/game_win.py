de6bd28519f8c9982319e0b9d6d565e17b60fc33
Zach Arnett <88991823+Zach-Arnett@users.noreply.github.com>
2024-10-22 20:35:45 -0400

    Made scaling win screen easier

--- SnakeEyes/Code/Scenes/game_win.py
+++ SnakeEyes/Code/Scenes/game_win.py
@@ -80,14 +80,16 @@ class GameWin:
         spaceBetween = 10
         textPadding = 0.2
 
-        winnerWidth = 375
+        wantedTextSize = 60
+        titleTextSize = 22
+        scoreTextSize = 50
+        crimeTextSize = 25
+
+        winnerWidth = 360
         winnerHeight = 450
-        secondWidth = winnerWidth*(5/6)
-        secondHeight = winnerHeight*(5/6)
-        thirdWidth = winnerWidth*(2/3)
-        thirdHeight = winnerHeight*(2/3)
-        fourthWidth = winnerWidth*(2/5)
-        fourthHeight = winnerHeight*(2/5)
+        secondScale = (5/6)
+        thirdScale = (4/6)
+        fourthScale = (3/6)
 
         pCount = len(self.game.Players)
 
@@ -98,14 +100,14 @@ class GameWin:
             pygame.draw.rect(self.screen, (200, 200, 200), winner_rect)
 
             winner_text = "WANTED"
-            currentFontSize = 60
+            currentFontSize = wantedTextSize
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             winner_wanted_text_rect = currentFont.get_rect(winner_text)
             winner_wanted_text_rect.midtop = (winner_rect.centerx, winner_rect.top + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, winner_wanted_text_rect, winner_text, (0, 0, 0))
             
             winner_text = f"CRIMINAL MASTERMIND: P{self.sorted_players[0].playerNum}"
-            currentFontSize = 23
+            currentFontSize = titleTextSize
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             winner_title_text_rect = currentFont.get_rect(winner_text)
             winner_title_text_rect.midtop = (winner_wanted_text_rect.centerx, winner_wanted_text_rect.bottom + (currentFontSize*textPadding))
@@ -120,14 +122,14 @@ class GameWin:
             self.screen.blit(image, (image_x, image_y))
 
             winner_text = f"${self.sorted_players[0].score:,.2f}"
-            currentFontSize = 50
+            currentFontSize = scoreTextSize
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             winner_score_rect = currentFont.get_rect(winner_text)
             winner_score_rect.midbottom = (winner_rect.centerx, winner_rect.bottom - (currentFontSize*textPadding))
             currentFont.render_to(self.screen, winner_score_rect, winner_text, (0, 0, 0))
             
             winner_text = "For the Theft of"
-            currentFontSize = 25
+            currentFontSize = crimeTextSize
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             winner_crime_rect = currentFont.get_rect(winner_text)
             winner_crime_rect.midbottom = (winner_score_rect.centerx, winner_score_rect.top - (currentFontSize*textPadding))
@@ -135,26 +137,26 @@ class GameWin:
         
         #2nd place
         if pCount >= 2:
-            second_rect = pygame.Rect(0, 0, secondWidth, secondHeight) #x, y, width, height
+            second_rect = pygame.Rect(0, 0, winnerWidth*secondScale, winnerHeight*secondScale) #x, y, width, height
             second_rect.bottomright = (winner_rect.left - spaceBetween, winner_rect.bottom)
             pygame.draw.rect(self.screen, (200, 200, 200), second_rect)  
 
             second_text = "WANTED"
-            currentFontSize = 50
+            currentFontSize = wantedTextSize*secondScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             second_wanted_text_rect = currentFont.get_rect(second_text)
             second_wanted_text_rect.midtop = (second_rect.centerx, second_rect.top + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, second_wanted_text_rect, second_text, (0, 0, 0))
 
             second_text = f"ROBBER: P{self.sorted_players[1].playerNum}"
-            currentFontSize = 19
+            currentFontSize = titleTextSize*secondScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             second_title_text_rect = currentFont.get_rect(second_text)
             second_title_text_rect.midtop = (second_wanted_text_rect.centerx, second_wanted_text_rect.bottom + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, second_title_text_rect, second_text, (0, 0, 0))
 
             image = pygame.image.load(self.game.character_sprites[self.sorted_players[1].character]["profile"])
-            image_size = secondWidth * 0.7
+            image_size = winnerWidth * secondScale * 0.7
             image = pygame.transform.scale(image, (image_size, image_size))
             image_x, image_y = (second_rect.centerx - (image_size/2), second_rect.centery - (image_size/2))
             image_back = pygame.Rect(image_x, image_y, image_size, image_size) #x, y,width, height
@@ -162,14 +164,14 @@ class GameWin:
             self.screen.blit(image, (image_x, image_y))
 
             second_text = f"${self.sorted_players[1].score:,.2f}"
-            currentFontSize = 41
+            currentFontSize = scoreTextSize*secondScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             second_score_rect = currentFont.get_rect(second_text)
             second_score_rect.midbottom = (second_rect.centerx, second_rect.bottom - (currentFontSize*textPadding))
             currentFont.render_to(self.screen, second_score_rect, second_text, (0, 0, 0))
             
             second_text = "For the Theft of"
-            currentFontSize = 20
+            currentFontSize = crimeTextSize*secondScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             second_crime_rect = currentFont.get_rect(second_text)
             second_crime_rect.midbottom = (second_score_rect.centerx, second_score_rect.top - (currentFontSize*textPadding))
@@ -177,26 +179,26 @@ class GameWin:
         
         #3rd place
         if pCount >= 3:
-            third_rect = pygame.Rect(0, 0, thirdWidth, thirdHeight) #x, y, width, height
+            third_rect = pygame.Rect(0, 0, winnerWidth*thirdScale, winnerHeight*thirdScale) #x, y, width, height
             third_rect.bottomleft = (winner_rect.right + spaceBetween, winner_rect.bottom)
             pygame.draw.rect(self.screen, (200, 200, 200), third_rect)  
 
             third_text = "WANTED"
-            currentFontSize = 40
+            currentFontSize = wantedTextSize*thirdScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             third_wanted_text_rect = currentFont.get_rect(third_text)
             third_wanted_text_rect.midtop = (third_rect.centerx, third_rect.top + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, third_wanted_text_rect, third_text, (0, 0, 0))
 
             third_text = f"BURGLAR: P{self.sorted_players[2].playerNum}"
-            currentFontSize = 15
+            currentFontSize = titleTextSize*thirdScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             third_title_text_rect = currentFont.get_rect(third_text)
             third_title_text_rect.midtop = (third_wanted_text_rect.centerx, third_wanted_text_rect.bottom + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, third_title_text_rect, third_text, (0, 0, 0))
 
             image = pygame.image.load(self.game.character_sprites[self.sorted_players[2].character]["profile"])
-            image_size = thirdWidth * 0.7
+            image_size = winnerWidth * thirdScale * 0.7
             image = pygame.transform.scale(image, (image_size, image_size))
             image_x, image_y = (third_rect.centerx - (image_size/2), third_rect.centery - (image_size/2))
             image_back = pygame.Rect(image_x, image_y, image_size, image_size) #x, y,width, height
@@ -204,14 +206,14 @@ class GameWin:
             self.screen.blit(image, (image_x, image_y))
 
             third_text = f"${self.sorted_players[2].score:,.2f}"
-            currentFontSize = 33
+            currentFontSize = scoreTextSize*thirdScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             third_score_rect = currentFont.get_rect(third_text)
             third_score_rect.midbottom = (third_rect.centerx, third_rect.bottom - (currentFontSize*textPadding))
             currentFont.render_to(self.screen, third_score_rect, third_text, (0, 0, 0))
             
             third_text = "For the Theft of"
-            currentFontSize = 16
+            currentFontSize = crimeTextSize*thirdScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             third_crime_rect = currentFont.get_rect(third_text)
             third_crime_rect.midbottom = (third_score_rect.centerx, third_score_rect.top - (currentFontSize*textPadding))
@@ -219,26 +221,26 @@ class GameWin:
         
         #4th place
         if pCount >= 4:
-            fourth_rect = pygame.Rect(0, 0, fourthWidth, fourthHeight) #x, y, width, height
+            fourth_rect = pygame.Rect(0, 0, winnerWidth*fourthScale, winnerHeight*fourthScale) #x, y, width, height
             fourth_rect.bottomleft = (third_rect.right + spaceBetween, third_rect.bottom)
             pygame.draw.rect(self.screen, (200, 200, 200), fourth_rect)  
 
             fourth_text = "WANTED"
-            currentFontSize = 24
+            currentFontSize = wantedTextSize*fourthScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             fourth_wanted_text_rect = currentFont.get_rect(fourth_text)
             fourth_wanted_text_rect.midtop = (fourth_rect.centerx, fourth_rect.top + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, fourth_wanted_text_rect, fourth_text, (0, 0, 0))
 
             fourth_text = f"PETTY THIEF: P{self.sorted_players[3].playerNum}"
-            currentFontSize = 9
+            currentFontSize = titleTextSize*fourthScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             fourth_title_text_rect = currentFont.get_rect(fourth_text)
             fourth_title_text_rect.midtop = (fourth_wanted_text_rect.centerx, fourth_wanted_text_rect.bottom + (currentFontSize*textPadding))
             currentFont.render_to(self.screen, fourth_title_text_rect, fourth_text, (0, 0, 0))
 
             image = pygame.image.load(self.game.character_sprites[self.sorted_players[3].character]["profile"])
-            image_size = fourthWidth * 0.7
+            image_size = winnerWidth * fourthScale * 0.7
             image = pygame.transform.scale(image, (image_size, image_size))
             image_x, image_y = (fourth_rect.centerx - (image_size/2), fourth_rect.centery - (image_size/2))
             image_back = pygame.Rect(image_x, image_y, image_size, image_size) #x, y,width, height
@@ -246,14 +248,14 @@ class GameWin:
             self.screen.blit(image, (image_x, image_y))
 
             fourth_text = f"${self.sorted_players[3].score:,.2f}"
-            currentFontSize = 20
+            currentFontSize = scoreTextSize*fourthScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             fourth_score_rect = currentFont.get_rect(fourth_text)
             fourth_score_rect.midbottom = (fourth_rect.centerx, fourth_rect.bottom - (currentFontSize*textPadding))
             currentFont.render_to(self.screen, fourth_score_rect, fourth_text, (0, 0, 0))
             
             fourth_text = "For the Theft of"
-            currentFontSize = 10
+            currentFontSize = crimeTextSize*fourthScale
             currentFont = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", currentFontSize)
             fourth_crime_rect = currentFont.get_rect(fourth_text)
             fourth_crime_rect.midbottom = (fourth_score_rect.centerx, fourth_score_rect.top - (currentFontSize*textPadding))
