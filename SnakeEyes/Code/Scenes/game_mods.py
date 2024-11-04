a3a6985896d7fe0a94e3df650e98d92011ffea00
isaachraga <isaachraga@yahoo.com>
2024-11-04 11:10:54 -0500

    current status

--- SnakeEyes/Code/Scenes/game_mods.py
+++ SnakeEyes/Code/Scenes/game_mods.py
@@ -1,7 +1,7 @@
 import pygame
 from SnakeEyes.Code.settings import Settings
-import modifier
-import textwrap
+from SnakeEyes.Code.modifier import Modifier
+#import textwrap
 
 class GameMods:
     def __init__(self, scene_manager, game):
@@ -9,7 +9,7 @@ class GameMods:
         self.game = game
         self.screen = self.scene_manager.screen
         self.GAME_FONT = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", Settings.FONT_SIZE)
-        self.available_mods = modifier.available_modifiers
+        self.available_mods = Modifier.available_modifiers
     
     ### Runs once when this scene is switched to ###
     def on_scene_enter(self):
@@ -30,13 +30,13 @@ class GameMods:
         for p in self.game.Players:
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 50), "Player "+str(p.playerNum)+": $"+str(p.score), Settings.COLOR_TEXT)
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 110), "Available Mods ", Settings.COLOR_TEXT)
-            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 150), modifier.available_modifiers[p.modSelection].name, Settings.COLOR_TEXT)
+            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 150), self.available_mods[p.modSelection].name, Settings.COLOR_TEXT)
             #self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 340), modifier.available_modifiers[p.modSelection].description, Settings.COLOR_TEXT)
-            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 190), "$"+str(modifier.available_modifiers[p.modSelection].cost), Settings.COLOR_TEXT)
+            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 190), "$"+str(self.available_mods[p.modSelection].cost), Settings.COLOR_TEXT)
             #self.drawText(self.screen, modifier.available_modifiers[p.modSelection].description, pygame.Rect(80+(300*(p.playerNum-1)), 340, 300, 300), self.GAME_FONT)
             offset = 0
             send = ''
-            for line in modifier.available_modifiers[p.modSelection].description:
+            for line in self.available_mods[p.modSelection].description:
                 send = send + line
                 if(line == '\n'):
                     self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 230+offset), send , Settings.COLOR_TEXT)
@@ -44,7 +44,7 @@ class GameMods:
                     send = ''
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 230+offset), send , Settings.COLOR_TEXT)
             offset = offset + 40
-            if modifier.available_modifiers[p.modSelection] in p.currentMods:
+            if self.available_mods[p.modSelection] in p.currentMods:
                 self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 230+offset), "PURCHASED" , Settings.COLOR_TEXT)
 
             offset = offset + 80
@@ -78,15 +78,15 @@ class GameMods:
                     if event.key == p.left:
                         #print(str(len(self.game.Players)))
                         if(p.modSelection - 1 < 0):
-                            p.modSelection = len(modifier.available_modifiers) -1 
+                            p.modSelection = len(self.available_mods) -1 
                         else:
                             p.modSelection = p.modSelection -1
                     if event.key == p.right:
-                        if(p.modSelection == len(modifier.available_modifiers) - 1):
+                        if(p.modSelection == len(self.available_mods) - 1):
                             p.modSelection = 0
                         else:
                             p.modSelection = p.modSelection + 1
 
-                    if event.key == p.ready and p.score >= modifier.available_modifiers[p.modSelection].cost and modifier.available_modifiers[p.modSelection] not in p.currentMods:
-                        p.score = p.score - modifier.available_modifiers[p.modSelection].cost
-                        p.currentMods[modifier.available_modifiers[p.modSelection]] = modifier.available_modifiers[p.modSelection]
+                    if event.key == p.ready and p.score >= self.available_mods[p.modSelection].cost and self.available_mods[p.modSelection] not in p.currentMods:
+                        p.score = p.score - self.available_mods[p.modSelection].cost
+                        p.currentMods[self.available_mods[p.modSelection]] = self.available_mods[p.modSelection]
