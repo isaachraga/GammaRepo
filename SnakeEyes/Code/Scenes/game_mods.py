2601b69711650ccd865aebe867697d7cf9751d6d
A-Obryant <Aobryant@kent.edu>
2024-11-13 16:31:40 -0500

    Adding images to modifiers

--- SnakeEyes/Code/Scenes/game_mods.py
+++ SnakeEyes/Code/Scenes/game_mods.py
@@ -10,6 +10,9 @@ class GameMods:
         self.screen = self.scene_manager.screen
         self.GAME_FONT = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", Settings.FONT_SIZE)
         self.available_mods = modifier.available_modifiers
+        self.charm = pygame.image.load('SnakeEyes/Assets/Icons/luckyStreakModifier.png')
+        self.dice = pygame.image.load('SnakeEyes/Assets/Icons/hotDiceModifier.png')
+        self.cash = pygame.image.load('SnakeEyes/Assets/Icons/cash.png')
     
     ### Runs once when this scene is switched to ###
     def on_scene_enter(self):
@@ -29,7 +32,10 @@ class GameMods:
 
         for p in self.game.Players:
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 50), "Player "+str(p.playerNum)+": $"+str(f'{p.score:,}'), Settings.COLOR_TEXT)
-            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 110), "Available Mods ", Settings.COLOR_TEXT)
+            self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 70), "Available Mods ", Settings.COLOR_TEXT)
+            self.screen.blit(self.charm, (75+(300*(p.playerNum-1)), 90))
+            self.screen.blit(self.cash, (130+(300*(p.playerNum-1)), 100))
+            self.screen.blit(self.dice, (175 + (300 * (p.playerNum - 1)), 85))
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 150), self.available_mods[p.modSelection].name, Settings.COLOR_TEXT)
             #self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 340), modifier.available_modifiers[p.modSelection].description, Settings.COLOR_TEXT)
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 190), "$"+str(f'{round(self.available_mods[p.modSelection].cost, 2):,}'), Settings.COLOR_TEXT)
