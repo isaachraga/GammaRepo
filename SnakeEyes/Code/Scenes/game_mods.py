41e0d9402d00cfbe46701fd7967a74c06934d14a
David-J-Tinley <dtinley@kent.edu>
2024-12-04 17:10:15 -0500

    Controller works for game mod and game status screens

--- SnakeEyes/Code/Scenes/game_mods.py
+++ SnakeEyes/Code/Scenes/game_mods.py
@@ -1,7 +1,7 @@
 import pygame
 from SnakeEyes.Code.settings import Settings
 from SnakeEyes.Code import modifier
-#import textwrap
+# import textwrap
 
 class GameMods:
     def __init__(self, scene_manager, game, Mult):
@@ -11,19 +11,17 @@ class GameMods:
         self.screen = self.scene_manager.screen
         self.GAME_FONT = pygame.freetype.Font("Fonts/HighlandGothicFLF-Bold.ttf", Settings.FONT_SIZE)
         self.available_mods = modifier.available_modifiers
-        
-    
+
     ### Runs once when this scene is switched to ###
     def on_scene_enter(self):
         self.scene_manager.play_music("SnakeEyes/Assets/Audio/Music/shopLoop.wav")
-    
+
     def run(self):
         self.update()
         self.render()
 
     def update(self):
         self.input_manager()
-        
 
     def render(self):
         self.screen.fill(Settings.COLOR_BACKGROUND)
@@ -34,9 +32,9 @@ class GameMods:
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 70), "Available Mods ", Settings.COLOR_TEXT)
             self.screen.blit(self.available_mods[p.modSelection].image, (75+(300*(p.playerNum-1)), 90))
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 150), self.available_mods[p.modSelection].name, Settings.COLOR_TEXT)
-            #self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 340), modifier.available_modifiers[p.modSelection].description, Settings.COLOR_TEXT)
+            # self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 340), modifier.available_modifiers[p.modSelection].description, Settings.COLOR_TEXT)
             self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 190), "$"+str(f'{round(self.available_mods[p.modSelection].cost, 2):,.{Settings.ROUNDING_PRECISION}f}'), Settings.COLOR_TEXT)
-            #self.drawText(self.screen, modifier.available_modifiers[p.modSelection].description, pygame.Rect(80+(300*(p.playerNum-1)), 340, 300, 300), self.GAME_FONT)
+            # self.drawText(self.screen, modifier.available_modifiers[p.modSelection].description, pygame.Rect(80+(300*(p.playerNum-1)), 340, 300, 300), self.GAME_FONT)
             offset = 0
             send = ''
             for line in self.available_mods[p.modSelection].description:
@@ -59,9 +57,7 @@ class GameMods:
                 modImg = pygame.transform.scale(m.image, (30,30))
                 self.screen.blit(modImg, (80+(300*(p.playerNum-1))+Xoffset, 230+offset))
                 Xoffset = Xoffset + 40
-                #self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 230+offset), m.image , Settings.COLOR_TEXT)
-
-            
+                # self.GAME_FONT.render_to(self.screen, (80+(300*(p.playerNum-1)), 230+offset), m.image , Settings.COLOR_TEXT)
 
         self.GAME_FONT.render_to(self.screen, (350, 680), "Press SPACE to continue...", Settings.COLOR_TEXT)
         pygame.display.flip()
@@ -69,17 +65,18 @@ class GameMods:
     def input_manager(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    self.scene_manager.quit()
+                self.scene_manager.quit()
 
             if event.type == pygame.KEYDOWN:
                 for p in self.game.Players:
-                    if p.controller.controller_type == "keyboard" or p.controller.controller_type == "joystick":
+
+                    if p.controller.controller_type == "keyboard":
                         if event.key == p.controller.left:
-                                #print(str(len(self.available_mods)))
-                                if(p.modSelection - 1 < 0):
-                                    p.modSelection = len(self.available_mods) -1 
-                                else:
-                                    p.modSelection = p.modSelection -1
+                            # print(str(len(self.available_mods)))
+                            if(p.modSelection - 1 < 0):
+                                p.modSelection = len(self.available_mods) -1 
+                            else:
+                                p.modSelection = p.modSelection -1
                         if event.key == p.controller.right:
                             if(p.modSelection == len(self.available_mods) - 1):
                                 p.modSelection = 0
@@ -93,7 +90,7 @@ class GameMods:
                                     self.scene_manager.switch_scene('mpause')
                                 else:
                                     self.scene_manager.switch_scene('pause')
-                            
+
                             if event.key == p.controller.action_buttons.get('space'):
                                 self.game.statusFlag = False
                                 if self.Mult:
@@ -104,12 +101,12 @@ class GameMods:
                             if event.key == p.controller.action_buttons.get('ready') and p.score >= self.available_mods[p.modSelection].cost and self.available_mods[p.modSelection] not in p.currentMods:
                                 p.score = round(p.score - self.available_mods[p.modSelection].cost)
                                 p.currentMods[self.available_mods[p.modSelection]] = self.available_mods[p.modSelection]
-            
+
             elif event.type == pygame.JOYBUTTONDOWN:
                 joystick_id = event.joy
                 button_id = event.button
 
-                for p in self.Players:
+                for p in self.game.Players:
                     if p.controller.controller_type == 'joystick':
                         if p.controller.joystick.get_instance_id() == joystick_id:
                             if button_id == p.controller.action_buttons.get('space'):
@@ -121,3 +118,14 @@ class GameMods:
                             elif button_id == p.controller.action_buttons.get('ready') and p.score >= self.available_mods[p.modSelection].cost and self.available_mods[p.modSelection] not in p.currentMods:
                                 p.score = round(p.score - self.available_mods[p.modSelection].cost)
                                 p.currentMods[self.available_mods[p.modSelection]] = self.available_mods[p.modSelection]
+                            elif button_id == p.controller.action_buttons.get("BLeft"):
+                                # print(str(len(self.available_mods)))
+                                if p.modSelection - 1 < 0:
+                                    p.modSelection = len(self.available_mods) - 1
+                                else:
+                                    p.modSelection = p.modSelection - 1
+                            elif button_id == p.controller.action_buttons.get("BRight"):
+                                if p.modSelection == len(self.available_mods) - 1:
+                                    p.modSelection = 0
+                            else:
+                                p.modSelection = p.modSelection + 1
--- SnakeEyes/Code/Scenes/game_status.py
+++ SnakeEyes/Code/Scenes/game_status.py
@@ -17,6 +17,7 @@ class GameStatus:
         self.clock = pygame.time.Clock() #Needed for pygame_gui
 
         self.make_GUI()
+        self.joystick = pygame.joystick.Joystick(0)
 
     def make_GUI(self):
         self.button_width = 500
@@ -39,11 +40,11 @@ class GameStatus:
             object_id='#back_panel',
             manager=self.ui_manager
         )
-    
+
     ### Runs once when this scene is switched to ###
     def on_scene_enter(self):
         self.scene_manager.play_music("SnakeEyes/Assets/Audio/Music/shopLoop.wav")
-    
+
     def run(self):
         self.time_delta = self.clock.tick(60) / 1000.0 #Needed for pygame_gui
         self.update()
@@ -52,7 +53,7 @@ class GameStatus:
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    self.scene_manager.quit()
+                self.scene_manager.quit()
 
             self.ui_manager.process_events(event) #Update pygame_gui
 
@@ -64,14 +65,18 @@ class GameStatus:
                         self.scene_manager.switch_scene('mpause')
                     else:
                         self.scene_manager.switch_scene('pause')
-            
+
             if event.type == pygame.USEREVENT:
-                #Check if a button was clicked
+                # Check if a button was clicked
                 if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                    #Continue Button
+                    # Continue Button
                     if event.ui_element == self.continue_button:
                         self.next_scene()
                         self.scene_manager.play_sound("SnakeEyes/Assets/Audio/SFX/blipSelect.wav")
+
+            if event.type == pygame.JOYBUTTONDOWN:
+                if self.joystick.get_button(1):
+                    self.next_scene()
 
     def next_scene(self):
         if Preferences.MODS_PREFERENCE == "Enabled":
@@ -93,11 +98,11 @@ class GameStatus:
         status_text_rect.center = ((Settings.WIDTH / 2), Settings.HEADER_FONT_SIZE)
         self.HEADER_FONT.render_to(self.screen, status_text_rect, "GAME STATUS", Settings.COLOR_TEXT)
 
-        #Render pygame_gui
+        # Render pygame_gui
         self.ui_manager.update(self.time_delta)
         self.ui_manager.draw_ui(self.screen)    
 
-        #Score
+        # Score
         pCount = len(self.game.Players)
 
         VerticalPadding = 100
@@ -110,12 +115,12 @@ class GameStatus:
         spaceForBars = totalHeight - (bufferBetween * (pCount - 1))
         barHeight = spaceForBars / pCount
         goalBarWidth = 3
-        
-        #Goal Bar
+
+        # Goal Bar
         goal_rect = pygame.Rect(barRight, VerticalPadding, goalBarWidth, totalHeight) #x, y, width, height
         pygame.draw.rect(self.screen, Settings.COLOR_TEXT, goal_rect)
 
-        #Intermediate bars
+        # Intermediate bars
         intermediate_rect = pygame.Rect(barRight - ((barGoalWidth/4)*1), VerticalPadding, goalBarWidth, totalHeight) #x, y, width, height
         pygame.draw.rect(self.screen, Settings.COLOR_ACCENT, intermediate_rect)
         intermediate_rect = pygame.Rect(barRight - ((barGoalWidth/4)*2), VerticalPadding, goalBarWidth, totalHeight) #x, y, width, height
@@ -123,28 +128,28 @@ class GameStatus:
         intermediate_rect = pygame.Rect(barRight - ((barGoalWidth/4)*3), VerticalPadding, goalBarWidth, totalHeight) #x, y, width, height
         pygame.draw.rect(self.screen, Settings.COLOR_ACCENT, intermediate_rect)
 
-        #Player Bars
+        # Player Bars
         pCurIndex = 1
         for p in self.game.Players:
-            #Handle decimals
+            # Handle decimals
             pScore = float(self.game.getScore(p.playerNum))
             if (Settings.ROUNDING_PRECISION != 0):
                 pScore = round(pScore, Settings.ROUNDING_PRECISION)
             else:
                 pScore = int(pScore)
 
-            #Score Bar
+            # Score Bar
             curBarWidth = minBarWidth + (barGoalWidth * (pScore / Preferences.FINISHLINE_SCORE))
             # curBarWidth = minBarWidth + barGoalWidth #DEBUG
             rect = pygame.Rect( barLeft, VerticalPadding+((pCurIndex-1)*(barHeight + bufferBetween)), curBarWidth, barHeight) #x, y, width, height
             pygame.draw.rect(self.screen, p.color, rect)
-            
-            #Text
+
+            # Text
             player_num_text = "P"+str(p.playerNum)
             player_num_rect = self.GAME_FONT.get_rect(player_num_text)
             player_num_rect.midright = (rect.left - 5, rect.centery - Settings.FONT_SIZE/2)
             self.GAME_FONT.render_to(self.screen, player_num_rect, player_num_text, Settings.COLOR_TEXT)
-            
+
             player_score_text = "$"+str(f'{pScore:,.{Settings.ROUNDING_PRECISION}f}')
             player_score_rect = self.GAME_FONT.get_rect(player_score_text)
             player_score_rect.midright = (rect.left - 5, rect.centery + Settings.FONT_SIZE/2)
@@ -152,7 +157,7 @@ class GameStatus:
 
             pCurIndex += 1
 
-        #Goal Text
+        # Goal Text
         goal_text = "Goal"
         goal_text_rect = self.GAME_FONT.get_rect(goal_text)
         goal_text_rect.midleft = (goal_rect.right + 5, goal_rect.centery - Settings.FONT_SIZE/2)
@@ -162,6 +167,5 @@ class GameStatus:
         goal_score_rect = self.GAME_FONT.get_rect(goal_score_text)
         goal_score_rect.midleft = (goal_rect.right + 5, goal_rect.centery + Settings.FONT_SIZE/2)
         self.GAME_FONT.render_to(self.screen, goal_score_rect, goal_score_text, Settings.COLOR_TEXT)
-
 
         pygame.display.flip()
